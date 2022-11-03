ed26dee0bfc80c0f77a552f9eb0841a0a914d47d
jserumaga <j.serumaga@gmail.com>
2022-11-03 11:05:44 -0700

    _print_listin.c

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+
+size_t print_listint(const listint_t *h);
+
+#endif /**SHELL_H*/
